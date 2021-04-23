a5c7d05de94588f903ad0f1ff8cc7e8af681358a
Cristiane Menna <cristianemenna@protonmail.com>
2021-04-23 23:10:38 +0200

    Adding @nuxt/content module and finishing basic setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'les-lianes',
     htmlAttrs: {
@@ -18,28 +16,27 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxt/content'
   ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./content/portfolio').map(file => {
+        return {
+          route: `/${path.parse(file).name}`,
+          payload: require(`./content/portfolio/${file}`),
+        };
+      });
+    },
+  },
+
 }
---
+++ pages/_slug.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .portfolio
+    h2 {{ post.title }}
+    nuxt-content(:document="post")
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let post;
+    try {
+      post = await $content("portfolio", params.slug).fetch();
+    } catch (e) {
+      error({ message: "Post not found" });
+    }
+
+    return {
+      post,
+    };
+  },
+};
+</script>
