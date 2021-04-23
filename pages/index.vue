9e0da8569e2a764604e84d124cc42bdd4cd1d2e3
Cristiane Menna <cristianemenna@protonmail.com>
2021-04-23 17:22:55 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    Collective
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Collective from './les-lianes.vue'
+
+@Component({
+  components: {
+    Collective,
+  }
+})
+export default class Index extends Vue {}
+</script>
+
+<style lang="less" scoped>
+
+
+</style>
