3cdf837d2273557d8a65fd8f3d89052c9cc81eb2
Cristiane Menna <cristianemenna@protonmail.com>
2021-06-18 13:56:41 +0200

    creating PortfolioFilteredByTag component

---
+++ components/portfolio/PortfolioFilteredByTag.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .portfolio-wrapper
+    template(v-for="post of portfolioFilteredByTag")
+      .portfolio-item
+        a(:href="'/nos-publications/' + post.slug")
+          img(:src="post.image")
+        p {{ post.title }}
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Portfolio extends Vue {
+  @Prop()
+  public tag!: string;
+
+  @Prop()
+  public portfolio!: any[];
+
+  get portfolioFilteredByTag() {
+    const portfolioWithTags = this.portfolio.filter(p => p.tag);
+    return portfolioWithTags.filter(p => p.tag.includes(this.tag));
+  }
+}
+</script>
