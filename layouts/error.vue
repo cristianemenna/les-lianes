ccd69714836a9e6f83bfd3960347f64b21cee054
Cristiane Menna <cristianemenna@protonmail.com>
2021-09-19 22:54:43 +0200

    adding style to error page and description meta-tag

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">Page inexistante</h1>
-    <h1 v-else>Nous avons trouvé une erreur.</h1>
-    <p>Cette page n'existe pas sur notre site</p>
-    <NuxtLink to="/">Retour sur la page d'accueil</NuxtLink>
+  <div class="main">
+    <div class="container">
+      <img id="logo" src="~/assets/img/logo.png" />
+      <div v-if="error.statusCode === 404">
+        <h1>Page inexistante</h1>
+        <p>Cette page n'existe pas sur notre site</p>
+      </div>
+      <h1 v-else>Nous avons trouvé une erreur.</h1>
+      <NuxtLink to="/">Retour sur la page d'accueil</NuxtLink>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['error'],
-    layout: 'error' // you can set a custom layout for the error page
-  }
+export default {
+  props: ["error"],
+  layout: "error"
+};
 </script>
+
+<style lang="less" scoped>
+img {
+  width: 300px;
+}
+
+a {
+  &:hover {
+    cursor: pointer;
+    color: #228782;
+  }
+}
+
+.main {
+  grid-template-rows: 1fr 1fr;
+  display: grid;
+  grid-auto-flow: row;
+}
+
+.container {
+  display: grid;
+  grid-auto-flow: row;
+  justify-content: center;
+  align-content: center;
+  grid-row: 2;
+  text-align: center;
+  div, a {
+    margin-top: 30px;
+  }
+  a {
+    font-weight: bold;
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Les lianes est un collectif de journalistes et photographes. Sur notre site vous trouverez nos articles récents, nos publications et nos photos.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
